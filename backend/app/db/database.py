c5f498b8a048cd6eb5cabe9cf7c68dcb58430666
deepanjalivaday <deepanjalivaday@gmail.com>
2026-04-05 19:34:06 +0530

    env changes

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-load_dotenv(r"C:\Users\Avadh\Desktop\musu\food-app\.env")
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
